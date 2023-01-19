af9b7cc48726c9670121ffcfbfa34de7d38bd84b
Ahendrix9624 <33384698+Ahendrix9624@users.noreply.github.com>
2023-01-18 20:57:36 -0700

    init commit

--- example2.py
+++ example2.py
@@ -1,3 +1,33 @@
+#!/usr/bin/env python3
+#
+#  [Program]
+#
+#  Create Your Own API
+#
+#  [Author]
+#
+#  Drew, https://github.com/Ahendrix9624/
+#
+#  [License]
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 3 of the License, or
+#  any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+#  See 'LICENSE' for more information.
+
+
+
 from fastapi import FastAPI, Path, Query, HTTPException, status
 from typing import Optional
 from pydantic import BaseModel
